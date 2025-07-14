78ce145e638ae9cbbb48bfa5ff758f0808051c08
Dhruv Bheda <dhruvbheda009@gmail.com>
2025-07-14 22:22:35 +0530

    Add Master Page Layout in ASP.NET Core

---
+++ Chp7 - Master Layout in DotNetCore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Chp7___Master_Layout_in_DotNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Chp7___Master_Layout_in_DotNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Chp7 - Master Layout in DotNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chp7___Master_Layout_in_DotNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
